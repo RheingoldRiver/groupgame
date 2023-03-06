30e6143a555ca4009519f217e8c50a766dddad16
River <18037011+RheingoldRiver@users.noreply.github.com>
2023-03-06 12:33:31 -0600

    Make a deck (#3)

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,42 @@
+import { range } from "lodash";
+import { useState } from "react";
+import { getRandomInt } from "../../utils";
 import { Board } from "../Board/Board";
 
+function addAttribute(numLeft: number, deck: number[][]) {
+  const newDeck = [];
+  if (numLeft === 1) {
+    range(2).map((i: number) => {
+      deck.map((card: number[]) => {
+        newDeck.push([i, [...card]]);
+      });
+    });
+    return deck;
+  } else {
+    addAttribute(numLeft - 1, deck);
+  }
+}
+
+function shuffleDeck(deck: number[][]) {
+  const max = deck.length;
+  for (let i = 0; i === 100; i++) {
+    const a = getRandomInt(max);
+    const b = getRandomInt(max);
+    const card1 = deck[a];
+    const card2 = deck[b];
+    deck[a] = card2;
+    deck[b] = card1;
+  }
+}
+
 export const Game = ({}: {}) => {
+  const [deck, setDeck] = useState(() => {
+    const deck = addAttribute(4, []);
+    // make the type checker happy since addAttribute is recursive
+    if (deck === undefined) return undefined;
+    shuffleDeck(deck);
+    return deck;
+  });
   return (
     <div className="grid justify-center py-4">
       <Board></Board>
--- src/components/GameStateProvider/GameStateProvider.tsx
+++ src/components/GameStateProvider/GameStateProvider.tsx
@@ -1,7 +1,52 @@
 import { createContext, ReactNode } from "react";
+import { range } from "lodash";
+import { useState } from "react";
+import { getRandomInt } from "../../utils";
 
-export const GameStateContext = createContext({});
+interface GameState {
+  deck: number[][];
+}
+
+const DEFAULT_GAME_STATE: GameState = {
+  deck: [],
+};
+
+export const GameStateContext = createContext(DEFAULT_GAME_STATE);
+
+function addAttribute(numLeft: number, deck: number[][]): number[][] {
+  if (numLeft === 0) {
+    return deck;
+  }
+  const newDeck: number[][] = [];
+  range(3).map((i: number) => {
+    deck.map((card: number[]) => {
+      newDeck.push([i, ...card]);
+    });
+  });
+  return addAttribute(numLeft - 1, newDeck);
+}
+
+function shuffleDeck(deck: number[][]) {
+  const max = deck.length;
+  for (let k = 0; k < 100; k++) {
+    const a = getRandomInt(max);
+    const b = getRandomInt(max);
+    const card1 = deck[a];
+    const card2 = deck[b];
+    deck[a] = card2;
+    deck[b] = card1;
+  }
+}
 
 export function GameStateProvider({ children }: { children: ReactNode }) {
-  return <GameStateContext.Provider value={{}}>{children}</GameStateContext.Provider>;
+  const [deck, setDeck] = useState<number[][]>(() => {
+    const baseDeck = [[0], [1], [2]];
+    const tempDeck = addAttribute(3, baseDeck);
+    // make the type checker happy since addAttribute is recursive
+    if (tempDeck === undefined) return baseDeck;
+    shuffleDeck(tempDeck);
+    return tempDeck;
+  });
+
+  return <GameStateContext.Provider value={{ deck }}>{children}</GameStateContext.Provider>;
 }
