a9562e105f5084105b420cbcb2e76999ee86635e
River <18037011+RheingoldRiver@users.noreply.github.com>
2023-03-06 02:48:40 -0600

    Still stateless but now there are cards (#2)

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,11 +1,15 @@
 import { range } from "lodash";
 import { Card } from "../Card/Card";
 
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export const Board = ({}: {}) => {
   return (
     <div className="grid grid-rows-3 grid-cols-4 gap-x-3 gap-y-3 max-w-[50rem]">
       {range(12).map((i) => (
-        <Card key={i}></Card>
+        <Card key={i} cardId={[getRandomInt(3), getRandomInt(3), getRandomInt(3), getRandomInt(3)]}></Card>
       ))}
     </div>
   );
